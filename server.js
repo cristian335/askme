e27f09b47ceb98cb8ed94818a2b3b7c7c966fd00
cristian335 <cristian-zamfir@outlook.com>
2018-10-16 20:50:47 +0200

    Emit newMessage on createMessage

--- server.js
+++ server.js
@@ -103,13 +103,13 @@ app.get('/chat', (req,res)=>{
 io.on('connection', function (socket) {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text:'See you then'
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text:message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
